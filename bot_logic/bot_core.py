9bd60c2b98280f23f7ce855d080faae56e4ea009
Unknown <rangers1500@yahoo.com>
2018-08-14 00:10:59 -0700

    Update bot to rewrite
    Update bot to to use python 3.7, use discord.py rewrite, debug bot.

--- bot_logic/bot_core.py
+++ bot_logic/bot_core.py
@@ -15,24 +15,25 @@ There are a number of utility commands being showcased here.'''
 bot = commands.Bot(command_prefix='?', description=description)
 
 
-@bot.event()
+@bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
 
 
-@bot.event()
+@bot.event
 async def on_message(message : discord.Message):
     if bot.user in message.mentions:
-        bot.say(message.author.mention)
+        await message.channel.send(message.author.mention)
         
     await bot.process_commands(message)
 
 
-@bot.command()
-async def hello():
-    await bot.say('Hello!')
+@bot.command
+async def hello(ctx):
+    await ctx.send('Hello!')
 
 
+#bot.run("Token here")
 with open('token.txt') as f:
     bot.run(f.readline())
