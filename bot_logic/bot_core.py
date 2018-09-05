8aa559fa317b6757b211b5cf839f63d779159d0d
Unknown <rangers1500@yahoo.com>
2018-09-04 23:47:25 -0700

    Allow the bot to ping others when pinged.

--- bot_logic/bot_core.py
+++ bot_logic/bot_core.py
@@ -13,7 +13,7 @@ module.
 There are a number of utility commands being showcased here.'''
 
 bot = commands.Bot(command_prefix='?', description=description)
-
+guildName = "pingable"
 
 @bot.event
 async def on_ready():
@@ -23,8 +23,15 @@ async def on_ready():
 
 @bot.event
 async def on_message(message : discord.Message):
-    if bot.user in message.mentions:
-        await message.channel.send(message.author.mention)
+    if bot.user.mentioned_in(message):
+        atList = [ m.mention for m in message.mentions if m.mention != bot.user.mention]
+        if len(atList) > 0:
+            if guildName in [r.name for r in message.author.roles]:
+                await message.channel.send(" ".join(atList))
+            else:
+                await message.channel.send("You need the correct role to use this feature, please contact the bot administrator.")
+        else:
+            await message.channel.send(message.author.mention)
         
     await bot.process_commands(message)
 
